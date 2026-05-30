0f6c21029b6bdaa203037f089f54c158999732e5
Andrés García Navarro <andrga37@ucm.es>
2026-05-30 14:32:56 +0200

    icons

--- skylanders/addons/godot_4d/godot_4d.gd
+++ skylanders/addons/godot_4d/godot_4d.gd
@@ -1,14 +1,17 @@
 @tool
 extends EditorPlugin
+const NODE_4D = preload("res://addons/godot_4d/icons/Node4D.svg")
+const MESH_INSTANCE_4D = preload("res://addons/godot_4d/icons/MeshInstance4D.svg")
+const COLLISION_SHAPE_4D = preload("res://addons/godot_4d/icons/CollisionShape4D.svg")
+const RIGIDBODY_4D = preload("res://addons/godot_4d/icons/Rigidbody4D.svg")
 
 func _enter_tree():
 	# Registrar todos los iconos
-	var mesh_instance_4d = preload("res://addons/godot_4d/icons/MeshInstance4D.svg")
-	var node4d_icon = preload("res://addons/godot_4d/icons/Node4D.svg")
-	
 	var theme = get_editor_interface().get_editor_theme()
-	theme.set_icon("MeshInstance4D", "EditorIcons", mesh_instance_4d)
-	theme.set_icon("Node4D", "EditorIcons", node4d_icon)
+	theme.set_icon("Node4D", "EditorIcons", NODE_4D)
+	theme.set_icon("MeshInstance4D", "EditorIcons", MESH_INSTANCE_4D)
+	theme.set_icon("CollisionShape4D", "EditorIcons", COLLISION_SHAPE_4D)
+	theme.set_icon("RigidBody4D", "EditorIcons", RIGIDBODY_4D)
 
 func _exit_tree():
 	pass
