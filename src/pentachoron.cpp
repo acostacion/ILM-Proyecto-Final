19acdb824e306974b821c9a54ba8e102e280009c
acostacion <carmengb27@ucm.es>
2026-05-31 10:13:06 +0200

    creo k estan las caras del pentacoro fixed

--- src/pentachoron.cpp
+++ src/pentachoron.cpp
@@ -19,11 +19,11 @@ void godot::Pentachoron::_bind_methods() {
 
 void godot::Pentachoron::_generate_vertices() {
     // https://polytope.miraheze.org/wiki/Pentachoron
-    vertices.push_back({0, Vector4(1.0f/2.0f, -sqrt(3.0f)/6.0f, -sqrt(6.0f)/12.0f, -sqrt(10.0f)/20.0f)});
+    vertices.push_back({0, Vector4(1.0f/2.0f,  -sqrt(3.0f)/6.0f, -sqrt(6.0f)/12.0f, -sqrt(10.0f)/20.0f)});
     vertices.push_back({1, Vector4(-1.0f/2.0f, -sqrt(3.0f)/6.0f, -sqrt(6.0f)/12.0f, -sqrt(10.0f)/20.0f)});
-    vertices.push_back({2, Vector4(0.0f, sqrt(3.0f)/3.0f, -sqrt(6.0f)/12.0f, -sqrt(10.0f)/20.0f)});
-    vertices.push_back({3, Vector4(0.0f, 0.0f, sqrt(6.0f)/4.0f, -sqrt(10.0f)/20.0f)});
-    vertices.push_back({4, Vector4(0.0f, 0.0f, 0.0f, sqrt(10.0f)/5.0f)});
+    vertices.push_back({2, Vector4(0.0f,        sqrt(3.0f)/3.0f, -sqrt(6.0f)/12.0f, -sqrt(10.0f)/20.0f)});
+    vertices.push_back({3, Vector4(0.0f,        0.0f,             sqrt(6.0f)/4.0f,  -sqrt(10.0f)/20.0f)});
+    vertices.push_back({4, Vector4(0.0f,        0.0f,             0.0f,              sqrt(10.0f)/5.0f)});
 }
 
 void godot::Pentachoron::_generate_faces() {
@@ -34,15 +34,15 @@ void godot::Pentachoron::_generate_faces() {
     faces.push_back({3, 0, 1});
     faces.push_back({3, 1, 2});
     faces.push_back({3, 2, 0});
-    faces.push_back({2, 0, 1});
+    faces.push_back({0, 2, 1});
 
     // ahora los triangulos qeu forman con el de dentro
-    faces.push_back({3, 0, 4});
-    faces.push_back({3, 4, 1});
+    faces.push_back({3, 4, 0});
+    faces.push_back({3, 1, 4});
     faces.push_back({3, 4, 2});
-    faces.push_back({4, 0, 1});
-    faces.push_back({4, 1, 2});
-    faces.push_back({4, 2, 0});
+    faces.push_back({4, 1, 8});
+    faces.push_back({4, 2, 1});
+    faces.push_back({4, 0, 2});
 }
 
 godot::Pentachoron::Pentachoron() : size(1, 1, 1, 1) {
